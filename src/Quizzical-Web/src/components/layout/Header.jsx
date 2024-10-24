61642f1d9f8f328c0f00034915153de1afcf74bd
Darkun <vasek.stancik@proton.me>
2024-10-24 09:39:10 +0200

    dropdown

--- src/Quizzical-Web/src/components/layout/Header.jsx
+++ src/Quizzical-Web/src/components/layout/Header.jsx
@@ -1,5 +1,15 @@
 ﻿import {Button} from "@/components/ui/button.jsx";
-import {Link, Outlet} from "react-router-dom";
+import {Link} from "react-router-dom";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 const Header = () => {
     return (
         <header className="border bg-card text-card-foreground shadow-sm flex justify-between p-2 border-l-0 border-r-0 border-t-0">
@@ -16,13 +26,28 @@ const Header = () => {
                     <Button variant="ghost">Tabulka</Button>
                 </Link>
             </div>
+
             <div className=" w-full flex justify-end gap-2">
-                <Link to="/login">
-                    <Button variant="ghost">Přihlášení</Button>
-                </Link>
-                <Link to="/register">
-                    <Button variant="ghost">Registrace</Button>
-                </Link>
+                {/*<Link to="/login">*/}
+                {/*    <Button variant="ghost">Přihlášení</Button>*/}
+                {/*</Link>*/}
+                {/*<Link to="/register">*/}
+                {/*    <Button variant="ghost">Registrace</Button>*/}
+                {/*</Link>*/}
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Button variant="ghost" className="text-primary">(JMÉNO UŽIVATELE)</Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem>
+                            <Link to="dashboard">Profil</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Odhlásit se</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
             </div>
         </header>
     )
--- src/Quizzical-Web/src/components/pages/dashboard/DashboardInfoPage.jsx
+++ src/Quizzical-Web/src/components/pages/dashboard/DashboardInfoPage.jsx
@@ -1,5 +1,43 @@
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.jsx";
+
 export const DashboardInfoPage = () => {
     return (
-        <h1>DashboardInfoPage</h1>
+        <div className="flex flex-col gap-8">
+            <h1>Vítejte (JMÉNO)</h1>
+            <div className="flex items-center justify-stretch gap-8">
+                <Card className="flex-1">
+                    <CardHeader>
+                        <CardTitle>Vytvořené Kvízy</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <p>6</p>
+                    </CardContent>
+                </Card>
+                <Card className="flex-1">
+                    <CardHeader>
+                        <CardTitle>Splněné Kvízy</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <p>215</p>
+                    </CardContent>
+                </Card >
+                <Card className="flex-1">
+                    <CardHeader>
+                        <CardTitle>Správné odpovědi</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <p>4562</p>
+                    </CardContent>
+                </Card>
+                <Card className="flex-1">
+                    <CardHeader>
+                        <CardTitle>Špatné odpovědi</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <p>89</p>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
     )
 }

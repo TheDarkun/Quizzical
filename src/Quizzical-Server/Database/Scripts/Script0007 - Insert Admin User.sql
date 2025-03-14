36c7bde00d5c4c7c308c33c3fc86ca1207a1228e
Darkun <106868917+TheDarkun@users.noreply.github.com>
2025-03-14 19:49:57 +0100

    Changed admin and basic user password

--- src/Quizzical-Server/Database/Scripts/Script0007 - Insert Admin User.sql
+++ src/Quizzical-Server/Database/Scripts/Script0007 - Insert Admin User.sql
@@ -1,13 +1,17 @@
 ﻿INSERT INTO user (email, password_hash, password_salt, is_admin)
 VALUES (
-    'admin@admin.cz',
-    'g7gELIkTisNsNPpZ/OCXl9yl+TIZdOpqHEdoJ0pPk4NKf7UBd8aFLmq+DjzGkhHQPf+RfSVH7GSUTvMMfcOnMQ==',
-    'eJGiyTiE3+K5bqKkp/G5Jw==',
+    'admin@email.cz',
+    'Tobm5N7nhc7IpNExTuHwYSLol/cVhUDjJLdeBZeIl0uapUGvW9YDEsajA0MEFuNgEyWNCsEC290yyws9jyVpdg==',
+    'LilMAdFL+NfHEGAF3cL7IQ==',
     1
 );
 
 INSERT INTO profile (user_id, name) 
 VALUES (
-    (SELECT id FROM user WHERE email = 'admin@admin.cz'),
+    (SELECT id FROM user WHERE email = 'admin@email.cz'),
     'admin'
 )
+
+-- name: admin
+-- email: admin@email.cz
+-- password: Heslo1234!
--- src/Quizzical-Server/Database/Scripts/Script0008 - Insert Basic User.sql
+++ src/Quizzical-Server/Database/Scripts/Script0008 - Insert Basic User.sql
@@ -1,12 +1,16 @@
 ﻿INSERT INTO user (email, password_hash, password_salt)
 VALUES (
-    'a@a.cz',
-    'g7gELIkTisNsNPpZ/OCXl9yl+TIZdOpqHEdoJ0pPk4NKf7UBd8aFLmq+DjzGkhHQPf+RfSVH7GSUTvMMfcOnMQ==',
-    'eJGiyTiE3+K5bqKkp/G5Jw=='
+    'a@email.cz',
+    'Tobm5N7nhc7IpNExTuHwYSLol/cVhUDjJLdeBZeIl0uapUGvW9YDEsajA0MEFuNgEyWNCsEC290yyws9jyVpdg==',
+    'LilMAdFL+NfHEGAF3cL7IQ=='
 );
 
 INSERT INTO profile (user_id, name)
 VALUES (
-   (SELECT id FROM user WHERE email = 'a@a.cz'),
+   (SELECT id FROM user WHERE email = 'a@email.cz'),
    'a'
 )
+
+-- name: a
+-- email: a@email.cz
+-- password: Heslo1234!
